65464254233aaee710080d5a494e62636e376f92
Gabriel Kazuo <kazuo.gabriel@gmail.com>
2024-12-06 20:26:15 -0300

    Initial Commit

---
+++ MyKsh/MyKshApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyKshApp.swift
+//  MyKsh
+//
+//  Created by Gabriel Nomura on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyKshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
