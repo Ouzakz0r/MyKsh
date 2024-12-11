006e4aa4ff742ae51895f56b33a2871a63567a07
Gabriel Kazuo <kazuo.gabriel@gmail.com>
2024-12-10 21:06:05 -0300

    Configurado o SwiftLint

--- MyKsh/MyKshApp.swift
+++ MyKsh/MyKshApp.swift
@@ -10,18 +10,17 @@ import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
     FirebaseApp.configure()
     return true
   }
 }
 
-
 @main
 struct MyKshApp: App {
-    //Registrando Firebase no appDelegate
+    // Registrando Firebase no appDelegate
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
